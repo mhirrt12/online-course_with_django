869cb5101aa491577986731a1eb2fc1477f7c53a
kalkidan <kaletamene90@gmail.com>
2026-08-14 05:57:33 -0700

    Format URL pattern in accounts app for better readability

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -1,3 +1,5 @@
 from . import views
 from django.urls import path
-urlpattern=[path("register/",views.register,name="register")]
+urlpattern=[
+    path("register/",views.register,name="register")
+    ]
