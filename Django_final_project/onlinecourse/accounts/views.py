779cb84fca70886d4d91f5b497a37c5572b6416c
kalkidan <kaletamene90@gmail.com>
2026-08-14 06:14:36 -0700

    Implement registration view with form handling and user creation

--- Django_final_project/onlinecourse/accounts/views.py
+++ Django_final_project/onlinecourse/accounts/views.py
@@ -1,9 +1,22 @@
-from http.client import HTTPResponse
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
-from django.shortcuts import render
 
-# Create your views here.
 def register(request):
-   return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            return redirect('login')
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
+
 def login(request):
    return render(request, 'accounts/login.html')
