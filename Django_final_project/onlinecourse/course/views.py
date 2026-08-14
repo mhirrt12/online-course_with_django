defed3e8480d8f659c527c0f6f4f1cb9af85fbd8
kalkidan <kaletamene90@gmail.com>
2026-08-14 04:13:23 -0700

    Add course_list view to retrieve and display courses

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from Django_final_project.onlinecourse.course.models import Course
+
 # Create your views here.
 
+def course_list(request):
+    # Logic to retrieve courses from the database
+    courses = Course.objects.all()
+    return render(request, 'course/course_list.html', {'courses': courses})
