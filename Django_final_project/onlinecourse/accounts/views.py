927f500aa1f03437d448cc2a09af149410793dfa
kalkidan <kaletamene90@gmail.com>
2026-08-14 06:02:27 -0700

    Add login view to accounts app

--- Django_final_project/onlinecourse/accounts/views.py
+++ Django_final_project/onlinecourse/accounts/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 def register(request):
    return render(request, 'accounts/register.html')
+def login(request):
+   return render(request, 'accounts/login.html')
