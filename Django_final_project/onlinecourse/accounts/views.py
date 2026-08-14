f6b26e579bed31dc85c17de6c8021aa91bbb7ab0
kalkidan <kaletamene90@gmail.com>
2026-08-14 05:41:21 -0700

    Add register view to accounts app

--- Django_final_project/onlinecourse/accounts/views.py
+++ Django_final_project/onlinecourse/accounts/views.py
@@ -1,3 +1,7 @@
+from http.client import HTTPResponse
+
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+   return HTTPResponse("This is the register view.")
