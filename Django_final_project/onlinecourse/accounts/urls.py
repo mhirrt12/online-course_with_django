0a6d743ca98f46960d6dde366d3e25c57bde14d6
kalkidan <kaletamene90@gmail.com>
2026-08-14 06:36:33 -0700

    Add dashboard URL pattern to accounts app

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 urlpatterns=[
     path("register/",views.register,name="register"),
-    path("login/",views.login,name="login")
+    path("login/",views.login,name="login"),
+    path("dashboard/",views.dashboard,name="dashboard")
     ]
