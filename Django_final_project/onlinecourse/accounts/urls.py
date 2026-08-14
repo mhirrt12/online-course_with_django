e0f326284bb558612eded17e33c5cdef9c161d41
kalkidan <kaletamene90@gmail.com>
2026-08-14 06:46:49 -0700

    Add logout URL pattern to accounts app

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 urlpatterns=[
     path("register/",views.register,name="register"),
     path("login/",views.login_view,name="login"),
-    path("dashboard/",views.dashboard,name="dashboard")
+    path("dashboard/",views.dashboard,name="dashboard"),
+    path('logout/', views.logout_view, name='logout'),
     ]
