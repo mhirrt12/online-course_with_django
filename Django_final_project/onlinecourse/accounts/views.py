5ec7328e9c7ef89f34c01ce625e99633babea041
kalkidan <kaletamene90@gmail.com>
2026-08-14 06:46:09 -0700

    Add logout view to handle user logout functionality

--- Django_final_project/onlinecourse/accounts/views.py
+++ Django_final_project/onlinecourse/accounts/views.py
@@ -47,3 +47,6 @@ def login_view(request):
 @login_required
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
+def logout_view(request):
+    logout(request)
+    return redirect('login')
