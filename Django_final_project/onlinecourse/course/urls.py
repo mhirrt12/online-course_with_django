be9a1afdb21f0fda0de97e4cd00c2b43ab26044a
kalkidan <kaletamene90@gmail.com>
2026-08-17 00:05:25 -0700

    Fix home view URL pattern to use correct path

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path('courses/', views.course_list, name='course_list'),
 ]
