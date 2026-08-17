f45e2d9539ff57e116f84464d74468cc8241454d
kalkidan <kaletamene90@gmail.com>
2026-08-16 23:53:47 -0700

    Add home view to display welcome message on the Online Course Platform

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -1,3 +1,5 @@
+from http.client import HTTPResponse
+
 from django.shortcuts import render
 
 from .models import Course
@@ -8,3 +10,5 @@ def course_list(request):
     # Logic to retrieve courses from the database
     courses = Course.objects.all()
     return render(request, 'courses/courses_list.html', {'courses': courses})
+def home(request):
+    return HTTPResponse("<h1>Welcome to the Online Course Platform</h1><p>Explore our courses and start learning today!</p>")
