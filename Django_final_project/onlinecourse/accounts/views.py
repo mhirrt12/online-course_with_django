0d94008ea54e8fb99ac475953c140c951048d729
kalkidan <kaletamene90@gmail.com>
2026-08-17 00:41:59 -0700

    Add edit profile functionality and template

--- Django_final_project/onlinecourse/accounts/views.py
+++ Django_final_project/onlinecourse/accounts/views.py
@@ -18,7 +18,19 @@ def register(request):
         form = RegistrationForm()
 
     return render(request, 'accounts/register.html', {'form': form})
+def edit_profile(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST, instance=request.user)
 
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            return redirect('dashboard')
+    else:
+        form = RegistrationForm(instance=request.user)
+    return render(request, 'accounts/edit_profile.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
