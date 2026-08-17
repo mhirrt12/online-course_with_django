9dca79fc76de86cdb7611145cdaa97cb15414860
kalkidan <kaletamene90@gmail.com>
2026-08-16 23:57:10 -0700

    Fix home view URL pattern in course URL configuration

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),
+    path("", views.home, name="home"),
+    path('courses/', views.course_list, name='course_list'),
 ]
