c57ec137750211d2813a98e181b2ca1dd2119c80
kalkidan <kaletamene90@gmail.com>
2026-08-17 00:17:23 -0700

    Update course_list URL pattern to use root path in urls.py

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('courses/', views.course_list, name='course_list'),
+    path('', views.course_list, name='course_list'),
 ]
