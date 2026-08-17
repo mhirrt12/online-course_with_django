6a65f4090edc40073d4f2a87f3c607f83bb2c761
kalkidan <kaletamene90@gmail.com>
2026-08-17 00:29:27 -0700

    Update registration form fields to include first and last name

--- Django_final_project/onlinecourse/accounts/forms.py
+++ Django_final_project/onlinecourse/accounts/forms.py
@@ -12,7 +12,7 @@ class RegistrationForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
 
     def clean(self):
         cleaned_data = super().clean()
