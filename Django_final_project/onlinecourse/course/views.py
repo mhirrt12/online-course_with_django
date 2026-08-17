f4c8596dfb9fb041b3d81721fcb41ee02500e124
kalkidan <kaletamene90@gmail.com>
2026-08-17 00:16:02 -0700

    Add home view to return welcome message in urls.py and remove redundant home function from views.py

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -10,5 +10,3 @@ def course_list(request):
     # Logic to retrieve courses from the database
     courses = Course.objects.all()
     return render(request, 'courses/courses_list.html', {'courses': courses})
-def home(request):
-    return HttpResponse("<h1>Welcome to the Online Course Platform</h1><p>Explore our courses and start learning today!</p>")
--- Django_final_project/onlinecourse/onlinecourse/urls.py
+++ Django_final_project/onlinecourse/onlinecourse/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
 # from ..course import views
-
+def home(request):
+    return HttpResponse("<h1>Welcome to the Online Course Platform</h1><p>Explore our courses and start learning today!</p>")
 urlpatterns = [
     # path('/',views.home,name='home'),
     path('admin/', admin.site.urls),
