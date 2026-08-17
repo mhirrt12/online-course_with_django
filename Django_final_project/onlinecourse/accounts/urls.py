018e7382ec170c3a2ad4ab466957f06622006b41
kalkidan <kaletamene90@gmail.com>
2026-08-17 02:47:04 -0700

    Add password reset confirmation URL to account routes

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -9,4 +9,11 @@ urlpatterns=[
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('change_password/', views.change_password, name='change_password'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path(
+    "password-reset-confirm/<uidb64>/<token>/",
+    auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_confirm.html"
+    ),
+    name="password_reset_confirm",
+),
     ]
