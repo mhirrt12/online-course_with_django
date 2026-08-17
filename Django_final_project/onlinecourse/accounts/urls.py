f9798847918f51736e96049b657532aed4ad8125
kalkidan <kaletamene90@gmail.com>
2026-08-17 01:01:00 -0700

    Add edit profile URL to urlpatterns

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path("login/",views.login_view,name="login"),
     path("dashboard/",views.dashboard,name="dashboard"),
     path('logout/', views.logout_view, name='logout'),
+    path('edit_profile/', views.edit_profile, name='edit_profile')
     ]
