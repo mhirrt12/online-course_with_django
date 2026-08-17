5b44823cd209219b2a41323ad2f9709e7e3d1d50
kalkidan <kaletamene90@gmail.com>
2026-08-17 03:03:05 -0700

    Refactor password reset URL structure for improved clarity

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -8,28 +8,18 @@ urlpatterns=[
     path('logout/', views.logout_view, name='logout'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('change_password/', views.change_password, name='change_password'),
-  # Password reset
-path(
-    "password_reset/",
-    auth_views.PasswordResetView.as_view(
-        template_name="accounts/password_reset.html"
-    ),
-    name="password_reset",
-),
-
-path(
-    "password-reset/done/",
-    auth_views.PasswordResetDoneView.as_view(
-        template_name="accounts/password_reset_done.html"
-    ),
-    name="password_reset_done",
-),
-
-path(
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path(
     "password-reset-confirm/<uidb64>/<token>/",
     auth_views.PasswordResetConfirmView.as_view(
         template_name="accounts/password_reset_confirm.html"
     ),
     name="password_reset_confirm",
+),path(
+    "password-reset/done/",
+    auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html"
+    ),
+    name="password_reset_done",
 ),
     ]
