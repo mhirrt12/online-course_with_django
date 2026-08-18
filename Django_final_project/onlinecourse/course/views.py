423bfd0ebccb67fe68be74bd3d6ac0024cd83a4e
kalkidan <kaletamene90@gmail.com>
2026-08-18 05:10:02 -0700

    Add course detail view for displaying individual course information

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -10,3 +10,6 @@ def course_list(request):
     # Logic to retrieve courses from the database
     courses = Course.objects.all()
     return render(request, 'courses/courses_list.html', {'course': courses})
+def coursedetail(request,id):
+    course = Course.objects.get(id=id)
+    return render(request,'courses/course_detail.html',{'course':course})
