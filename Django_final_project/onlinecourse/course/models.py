0c91dc09d89e26dffa53a74fdc2814e977816b7c
kalkidan <kaletamene90@gmail.com>
2026-08-18 05:50:02 -0700

    Add enroll field to Course model

--- Django_final_project/onlinecourse/course/models.py
+++ Django_final_project/onlinecourse/course/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    enroll=models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return self.title
