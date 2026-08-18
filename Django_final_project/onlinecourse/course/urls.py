781b3bb30a5ca046624907600aade4f3b7c7faea
kalkidan <kaletamene90@gmail.com>
2026-08-18 07:15:38 -0700

    Fix typo in URL pattern for 'mycourse'

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.course_list, name='course_list'),
     path("<int:id>",views.coursedetail,name="coursedetail"),
     path("enroll/<int:id>",views.enroll, name='enroll')
+    path("mycourse/",views.mycourse,name=mycourse)
 ]
