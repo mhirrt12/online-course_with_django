b61ecd0c2eb51b39aa50d54888f153c05b7fa4c8
kalkidan <kaletamene90@gmail.com>
2026-08-18 06:28:45 -0700

    Remove enroll field from Course model

--- Django_final_project/onlinecourse/course/models.py
+++ Django_final_project/onlinecourse/course/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import User
 # Create your models here.
 class Course(models.Model):
+    students =models.ManyToManyField(User)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    enroll=models.BooleanField()
+    # enroll=models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
