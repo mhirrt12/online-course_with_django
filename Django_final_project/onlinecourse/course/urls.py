17320b8e75d53ed59bf696250278d19f83f6bc3e
kalkidan <kaletamene90@gmail.com>
2026-08-18 05:06:46 -0700

    Update course URL pattern to remove course ID for simplified routing

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.course_list, name='course_list'),
+    
 ]
--- Django_final_project/onlinecourse/onlinecourse/urls.py
+++ Django_final_project/onlinecourse/onlinecourse/urls.py
@@ -24,7 +24,7 @@ def home(request):
 urlpatterns = [
     path('',home,name='home'),
     path('admin/', admin.site.urls),
-    path('courses/<int:id>/', include('course.urls')),
+    path('courses/', include('course.urls')),
     path("accounts/", include("accounts.urls")),
     # Include the course app's URLs
 ]
