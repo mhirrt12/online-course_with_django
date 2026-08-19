87cfeddc2ef4f5c7ecd880b44b698a2256d3d5c2
kalkidan <kaletamene90@gmail.com>
2026-08-19 01:34:23 -0700

    Fix category list view to retrieve both categories and courses

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -32,7 +32,8 @@ def category_list(request):
     if query:
         courses=Course.objects.filter(title__icontains=query)
     else:
-       categories = Category.objects.all()
+        courses = Course.objects.all()
+    categories = Category.objects.all()
     return render(request, 'courses/courses_list.html', {'categories': categories, 'courses': courses})
 def category_detail(request, id):
         
