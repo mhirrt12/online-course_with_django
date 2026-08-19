0d27c0251ce15f93038bf5f129916e374ba7b5dd
kalkidan <kaletamene90@gmail.com>
2026-08-18 23:48:28 -0700

    Fix typo in admin registration for Category model

--- Django_final_project/onlinecourse/course/admin.py
+++ Django_final_project/onlinecourse/course/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course,Catagory
+from .models import Course,Category
 
-admin.site.register(Course,Catagory)
+admin.site.register(Course,Category)
