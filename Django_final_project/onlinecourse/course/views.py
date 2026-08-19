ac759fad8dd6a36edcd097de7316e6f186a7828c
kalkidan <kaletamene90@gmail.com>
2026-08-19 00:12:06 -0700

    Add category retrieval to course list view for improved display

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -2,14 +2,15 @@ from django.http import HttpResponse
 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Course
+from .models import Course,Category
 
 # Create your views here.
 
 def course_list(request):
     # Logic to retrieve courses from the database
     courses = Course.objects.all()
-    return render(request, 'courses/courses_list.html', {'courses': courses})
+    category= Category.objects.all()
+    return render(request, 'courses/courses_list.html', {'courses': courses, 'categories': category})
 def coursedetail(request,id):
     course = Course.objects.get(id=id)
     return render(request,'courses/course_detail.html',{'course':course})
