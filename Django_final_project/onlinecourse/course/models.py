1bb5e1dc1cf5ca1998b595930980ef02eaa65c7a
kalkidan <kaletamene90@gmail.com>
2026-08-19 00:05:53 -0700

    Refactor Course and Category models to improve relationships and structure

--- Django_final_project/onlinecourse/course/models.py
+++ Django_final_project/onlinecourse/course/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Course(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
     students =models.ManyToManyField(User)
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -12,7 +13,7 @@ class Course(models.Model):
     def __str__(self):
         return self.title
 class Category(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+    
     name=models.CharField(max_length=100)
     description=models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
