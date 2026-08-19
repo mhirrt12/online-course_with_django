ee3514e0e99443cbf0cf25c7d7012fde41819257
kalkidan <kaletamene90@gmail.com>
2026-08-19 00:45:59 -0700

    Update root URL path to point to category_list view

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.course_list, name='course_list'),
+    path('', views.category_list, name='category_list'),
     path("<int:id>",views.coursedetail,name="coursedetail"),
     path("enroll/<int:id>",views.enroll,name='enroll'),
     path("mycourse/",views.mycourse,name='mycourse'),
