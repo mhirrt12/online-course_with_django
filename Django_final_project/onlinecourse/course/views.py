41c4aa4c3236dce3508d057456cebd2ad87aa026
kalkidan <kaletamene90@gmail.com>
2026-08-20 00:44:16 -0700

    Add lesson detail view to display individual lesson information

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -55,3 +55,6 @@ def category_detail(request, id):
     category = Category.objects.get(id=id)
     courses = Course.objects.filter(category=category)
     return render(request, 'courses/category_detail.html', {'category': category, 'courses': courses})
+def lesson_detail(request, id):
+    lesson_obj = lesson.objects.get(id=id)
+    return render(request, 'courses/lesson_detail.html', {'lesson': lesson_obj})
