15ac2e55228fb3f70f2a90fbbdf7d4fa396a8e87
kalkidan <kaletamene90@gmail.com>
2026-08-20 01:14:23 -0700

    Update mark_lesson_completed view to return a success message instead of redirecting

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -66,4 +66,4 @@ def mark_lesson_completed(request, id):
     if not lesson_obj.lessoncompletion_set.filter(user=user).exists():
         # Mark the lesson as completed for the user
         lesson_obj.lessoncompletion_set.create(user=user)
-    return redirect('course_detail', id=id)
+    return HttpResponse('Congratulations! You have completed the lesson.')
