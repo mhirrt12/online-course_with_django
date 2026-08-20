b5e766bb78257167d26f79efa63e017e6b884903
kalkidan <kaletamene90@gmail.com>
2026-08-20 00:43:17 -0700

    Add lesson detail URL pattern to urlpatterns

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("unenroll/<int:id>",views.unenroll,name='unenroll'),
     path('category/<int:id>/', views.category_detail, name='category_detail'),
     path("page/",views.course_list,name='course_list'),
+    path("lesson/<int:id>/", views.lesson_detail, name='lesson_detail')
 ]
