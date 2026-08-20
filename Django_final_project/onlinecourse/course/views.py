eabf272422d5ae34d0e84b44da32610ddfa786f6
kalkidan <kaletamene90@gmail.com>
2026-08-20 02:15:54 -0700

    Remove unused count variable from course detail view

--- Django_final_project/onlinecourse/course/views.py
+++ Django_final_project/onlinecourse/course/views.py
@@ -26,8 +26,8 @@ def coursedetail(request,id):
         form=ReviewForm()
     course = Course.objects.get(id=id)
     lessons = lesson.objects.filter(course=course)
-    count=0;
-    return render(request,'courses/course_detail.html',{'course':course, 'form': form,'lessons':lessons,'count':count})
+    
+    return render(request,'courses/course_detail.html',{'course':course, 'form': form,'lessons':lessons})
 @login_required
 def enroll(request,id):
     course=Course.objects.get(id=id)
