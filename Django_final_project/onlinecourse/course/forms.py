48a67be13c7f459f823eb19626b2348633c80020
kalkidan <kaletamene90@gmail.com>
2026-08-22 02:05:01 -0700

    Add CourseForm to handle Course model data in forms

--- Django_final_project/onlinecourse/course/forms.py
+++ Django_final_project/onlinecourse/course/forms.py
@@ -1,7 +1,15 @@
 from django import forms
-from .models import Review
+from .models import Review, Course
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ['rating', 'comment']
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = [
+            'title',
+            'description',
+            'category',
+        ]
