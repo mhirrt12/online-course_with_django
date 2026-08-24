ba2c27ce408fb262caa489bacefbad30d768e2bd
kalkidan <kaletamene90@gmail.com>
2026-08-24 00:58:48 -0700

    Add string representation method to Profile model

--- Django_final_project/onlinecourse/course/models.py
+++ Django_final_project/onlinecourse/course/models.py
@@ -84,3 +84,5 @@ class Profile(models.Model):
     ROLE_CHOICES=[('student','Student'),('instructor','Instructor')]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES,default='student')
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
