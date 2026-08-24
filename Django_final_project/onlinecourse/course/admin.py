8cda510d38ba225c4c3306337a60967a21dbad69
kalkidan <kaletamene90@gmail.com>
2026-08-24 01:56:52 -0700

    Register Profile model in admin and ensure newline at end of file

--- Django_final_project/onlinecourse/course/admin.py
+++ Django_final_project/onlinecourse/course/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Course,Category,Review, lesson
+from .models import Course,Category,Review, lesson,Profile
 
 admin.site.register(Course)
 admin.site.register(Category)
 admin.site.register(Review)
 admin.site.register(lesson)
+admin.site.register(Profile)
