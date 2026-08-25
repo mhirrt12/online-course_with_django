6c0eaac5abdd4f50d16ee286e55770f8e75beb93
kalkidan <kaletamene90@gmail.com>
2026-08-25 01:22:33 -0700

    Add URL pattern for deleting reviews in course detail

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -50,4 +50,5 @@ path(
 ,path("edit-lesson/<int:lesson_id>/", views.edit_lesson, name="edit_lesson"),
 path("delete-lesson/<int:lesson_id>/", views.delete_lesson, name="delete_lesson"),
 path("edit_review/<int:review_id>/",views.edit_review ,name="edit_review"),
+path("delete_review/<int:review_id>/",views.delete_review,name="delete_review"),
 ]
