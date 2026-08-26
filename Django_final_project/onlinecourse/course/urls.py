5f6ced1195b581a7b4a4cd7674d0c15ec1a58c94
kalkidan <kaletamene90@gmail.com>
2026-08-26 00:35:22 -0700

    Add URL pattern for my_learning view to display user's enrolled courses

--- Django_final_project/onlinecourse/course/urls.py
+++ Django_final_project/onlinecourse/course/urls.py
@@ -52,4 +52,9 @@ path(
 path("delete-lesson/<int:lesson_id>/", views.delete_lesson, name="delete_lesson"),
 path("edit_review/<int:review_id>/",views.edit_review ,name="edit_review"),
 path("delete_review/<int:review_id>/",views.delete_review,name="delete_review"),
+path(
+    'my-learning/',
+    views.my_learning,
+    name='my_learning'
+),
 ]
