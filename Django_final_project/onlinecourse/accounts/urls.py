68b28e7257309ee2667de033cc537d44033abd5a
kalkidan <kaletamene90@gmail.com>
2026-08-27 00:55:52 -0700

    Refactor URL patterns for improved readability and consistency

--- Django_final_project/onlinecourse/accounts/urls.py
+++ Django_final_project/onlinecourse/accounts/urls.py
@@ -1,20 +1,74 @@
 from . import views
 from django.urls import path
 from django.contrib.auth import views as auth_views
-urlpatterns=[
-    path("register/",views.register,name="register"),
-    path("login/",views.login_view,name="login"),
-    path("dashboard/",views.dashboard,name="dashboard"),
-    path('logout/', views.logout_view, name='logout'),
-    path('edit_profile/', views.edit_profile, name='edit_profile'),
-    path('change_password/', views.change_password, name='change_password'),
-    path('password_reset/', auth_views.PasswordResetView.as_view( template_name="accounts/password_reset.html"), name='password_reset'),
-    path("password-reset-confirm/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"),
-    name="password_reset_confirm",
-),
-    path("password-reset/done/",auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
-name="password_reset_done",
-),
-   path("password-reset-complete/",auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),
-    name="password_reset_complete",
-), ]
+
+urlpatterns = [
+
+    path(
+        "register/",
+        views.register,
+        name="register"
+    ),
+
+    path(
+        "login/",
+        views.login_view,
+        name="login"
+    ),
+
+    path(
+        "dashboard/",
+        views.dashboard,
+        name="dashboard"
+    ),
+
+    path(
+        "logout/",
+        views.logout_view,
+        name="logout"
+    ),
+
+    path(
+        "edit_profile/",
+        views.edit_profile,
+        name="edit_profile"
+    ),
+
+    path(
+        "change_password/",
+        views.change_password,
+        name="change_password"
+    ),
+
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="accounts/password_reset.html"
+        ),
+        name="password_reset"
+    ),
+
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done"
+    ),
+
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm"
+    ),
+
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete"
+    ),
+]
