d9179515513e3669f2c8ffa589bb694044ba6abd
kalkidan <kaletamene90@gmail.com>
2026-08-29 00:38:32 -0700

    Update DEBUG setting to use environment variable for production safety

--- Django_final_project/onlinecourse/onlinecourse/settings.py
+++ Django_final_project/onlinecourse/onlinecourse/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
