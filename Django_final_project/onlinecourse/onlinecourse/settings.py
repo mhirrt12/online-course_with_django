ec20524cec7162bf488e893f5d95c637436b5372
kalkidan <kaletamene90@gmail.com>
2026-08-29 00:36:35 -0700

    Refactor settings to use environment variable for SECRET_KEY

--- Django_final_project/onlinecourse/onlinecourse/settings.py
+++ Django_final_project/onlinecourse/onlinecourse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b$lvggvt2u+$iwm=*qxh1-ly*^lf2ai_85@@*70%d1bhh#ce0='
+
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY',
+    'django-development-only-key'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
