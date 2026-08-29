6f276fbac1d8b311e68a8638efc34010cb6245d7
kalkidan <kaletamene90@gmail.com>
2026-08-29 00:53:11 -0700

    Refactor database settings to use dj_database_url for better configuration management

--- Django_final_project/onlinecourse/onlinecourse/settings.py
+++ Django_final_project/onlinecourse/onlinecourse/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +83,12 @@ WSGI_APPLICATION = 'onlinecourse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
