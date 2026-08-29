ac4b19b60637a52015e2092e6dc52223cf7ccdd0
kalkidan <kaletamene90@gmail.com>
2026-08-29 00:18:43 -0700

    Prepare project for deployment

--- Django_final_project/onlinecourse/course/models.py
+++ Django_final_project/onlinecourse/course/models.py
@@ -42,6 +42,7 @@ class Review(models.Model):
         MaxValueValidator(5)
     ]
 )
+    
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
