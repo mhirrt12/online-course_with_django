f9c1031e2b35bf364afd1aec28f6766e6d0a52f0
kalkidan <kaletamene90@gmail.com>
2026-08-30 23:59:40 -0700

    Add missing newline at end of file in views.py

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from course.models import Course
+from .serializers import CourseSerializer
+
  
