9371a523c7c75e3feaba24b698781b4a9c2af0ca
kalkidan <kaletamene90@gmail.com>
2026-08-31 00:15:11 -0700

    Fix URL pattern for course API routing in urls.py

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[path('course/',views.course_list_api,name="course_list_api")]
+urlpatterns=[path('courses/',views.course_list_api,name="course_list_api")]
