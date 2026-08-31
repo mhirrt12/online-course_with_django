2d14fc5c85d579754896ffeb3dc01b633261183d
kalkidan <kaletamene90@gmail.com>
2026-08-30 23:57:32 -0700

    Fix import statement for api_view in views.py

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import api_view
+from rest_framework.decorators import api_view
+from rest_framework.response impo
