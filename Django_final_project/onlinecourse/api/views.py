72e4b5c89fc9785586d1e019d86e7efb3173f921
kalkidan <kaletamene90@gmail.com>
2026-08-30 23:58:41 -0700

    Fix formatting and add Course import in views.py

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from course.models import Course
+ 
