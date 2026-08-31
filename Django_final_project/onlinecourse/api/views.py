f15d4547a61a4cfc4a8382a5af383f77df9ab92b
kalkidan <kaletamene90@gmail.com>
2026-08-31 00:28:52 -0700

    Add category_list_api view to retrieve categories

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -3,11 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from course.models import Course
-from .serializers import CourseSerializer
+from course.models import Course,Category
+from .serializers import CourseSerializer,CategorySerializer
 
 @api_view(['GET'])
 def  course_list_api(request):
     courses= Course.objects.all()
     serialiser= CourseSerializer(courses, many=True)
     return Response(serialiser.data)
+
+@api_view(['GET'])
+def category_list_api(request):
+    category= Category.objects.all()
+    serializer=CategorySerializer(category, many=True)
+    return Response(serializer.data)
