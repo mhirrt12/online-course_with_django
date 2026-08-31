05d3ee2654c91dd47d6dc216f86e04f41cfa05ed
kalkidan <kaletamene90@gmail.com>
2026-08-31 00:38:44 -0700

    Add onecourse URL pattern to urlpatterns in urls.py

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
-             path('category/',views.category_list_api, name='categoty_list_api')]
+             path('category/',views.category_list_api, name='categoty_list_api'),
+             path('onecourse/',views.one_course, name='one_course')]
