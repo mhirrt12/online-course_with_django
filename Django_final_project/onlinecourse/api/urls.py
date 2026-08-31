34ba506c385fd89f4d9b3bdc6952443f1140f545
kalkidan <kaletamene90@gmail.com>
2026-08-31 00:30:33 -0700

    Add category URL pattern to urlpatterns in urls.py

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[path('courses/',views.course_list_api,name="course_list_api")]
+urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
+             path('category/',views.category_list_api, name='categoty_list_api')]
