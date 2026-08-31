ff392d1eae8265d18ec111a97578bf50d7dff9aa
kalkidan <kaletamene90@gmail.com>
2026-08-31 00:35:13 -0700

    Add one_course API view to retrieve the first course

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from course.models import Course,Category
-from .serializers import CourseSerializer,CategorySerializer
+from .serializers import CourseSerializer,CategorySerializer,CourseSerializer2
 
 @api_view(['GET'])
 def  course_list_api(request):
@@ -17,3 +17,8 @@ def category_list_api(request):
     category= Category.objects.all()
     serializer=CategorySerializer(category, many=True)
     return Response(serializer.data)
+@api_view(['GET'])
+def one_course(request):
+    course= Course.objects.first()
+    serializer= CourseSerializer2(course)
+    return Response(serializer)
