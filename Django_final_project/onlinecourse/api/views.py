ee0c21afa6cb8f65ce4fb44f0c6ac4cd5fc2792a
kalkidan <kaletamene90@gmail.com>
2026-09-01 00:34:10 -0700

    Add IsInstructor permission to course_list_api for access control

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -13,7 +13,7 @@ from rest_framework.permissions import IsAuthenticated
 from .permissions import IsInstructor
 
 @api_view(['GET','POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([IsAuthenticated,IsInstructor])
 def  course_list_api(request):
      if request.method=='GET':
        courses= Course.objects.all()
