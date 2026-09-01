e40ff3d7dcbdde1ee2af87eb645139b847b1a797
kalkidan <kaletamene90@gmail.com>
2026-09-01 01:37:24 -0700

    Add course detail API URL path for retrieving course details by ID

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
              path('category/',views.category_list_api, name='categoty_list_api'),
-             path('onecourse/',views.one_course, name='one_course')]
+             path('onecourse/',views.one_course, name='one_course'),path(
+    'courses/<int:id>/',
+    views.course_detail_api,
+    name='course_detail_api'
+)]
