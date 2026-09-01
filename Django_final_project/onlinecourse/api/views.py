7c67a8d28dd1c79e6c78c1c93520f5e661574671
kalkidan <kaletamene90@gmail.com>
2026-09-01 00:32:59 -0700

    Refactor imports in views.py for better organization

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from course.models import Course,Category
 from .serializers import CourseSerializer,CategorySerializer,CourseSerializer2
 
+from rest_framework.decorators import (
+    api_view,
+    permission_classes
+)
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsInstructor
 
 @api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
