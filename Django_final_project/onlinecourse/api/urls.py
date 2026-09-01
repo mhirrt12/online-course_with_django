5a2e462b5a2ced31e919f2ada14a8b5f4a408081
kalkidan <kaletamene90@gmail.com>
2026-09-01 01:51:40 -0700

    Update course detail API URL to use class-based view

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
              path('category/',views.category_list_api, name='categoty_list_api'),
-             path('onecourse/',views.one_course, name='one_course'),path(
+             path('onecourse/',views.one_course, name='one_course'),
+           path(
     'courses/<int:id>/',
-    views.course_detail_api,
+    views.CourseDetailAPIView.as_view(),
     name='course_detail_api'
 )]
