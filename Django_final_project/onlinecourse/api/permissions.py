0a0922720c9a463ae3befb4bce7e52db435858c8
kalkidan <kaletamene90@gmail.com>
2026-09-01 00:29:44 -0700

    Add IsInstructor permission to restrict access to instructors only

---
+++ Django_final_project/onlinecourse/api/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsInstructor(BasePermission):
+
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.profile.role == 'instructor'
+        )
