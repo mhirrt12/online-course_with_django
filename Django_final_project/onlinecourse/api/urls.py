e894d17f287436d81d5a5ea61db83223e65b0c71
kalkidan <kaletamene90@gmail.com>
2026-09-02 02:26:38 -0700

    Fix URL pattern for CourseDetailAPIView to use 'pk' instead of 'id'

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
              path('category/',views.category_list_api, name='categoty_list_api'),
              path('onecourse/',views.one_course, name='one_course'),
-           path('courses/<int:id>/',views.CourseDetailAPIView.as_view(),
-name='course_detail_api'
+          path(
+    'courses/<int:pk>/',
+    views.CourseDetailAPIView.as_view(),
+    name='course_detail_api'
 ),]
