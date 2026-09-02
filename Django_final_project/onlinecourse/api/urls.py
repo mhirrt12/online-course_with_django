57f15ace4fdc96456d180b9c44b1d40176332568
kalkidan <kaletamene90@gmail.com>
2026-09-02 04:34:23 -0700

    Refactor URL routing to use DefaultRouter for CourseViewSet and comment out unused urlpatterns

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns=[path(
-    'courses/',
-    views.CourseListCreateAPIView.as_view(),
-    name='course_list_create_api'
-),
-             path('category/',views.category_list_api, name='categoty_list_api'),
-             path('onecourse/',views.one_course, name='one_course'),
-          path('courses/<int:pk>/',views.CourseDetailAPIView.as_view(),name='course_detail_api'
-),]
+router = DefaultRouter()
+
+router.register('courses', views.CourseViewSet)
+# urlpatterns=[path(
+#     'courses/',
+#     views.CourseListCreateAPIView.as_view(),
+#     name='course_list_create_api'
+# ),
+#              path('category/',views.category_list_api, name='categoty_list_api'),
+#              path('onecourse/',views.one_course, name='one_course'),
+#           path('courses/<int:pk>/',views.CourseDetailAPIView.as_view(),name='course_detail_api'
+# ),]
