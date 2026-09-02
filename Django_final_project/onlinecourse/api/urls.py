a8e3ac3a1353c606c306eaf60f9a2826fae83039
kalkidan <kaletamene90@gmail.com>
2026-09-02 02:27:59 -0700

    Refactor URL pattern for CourseDetailAPIView for improved readability

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
              path('category/',views.category_list_api, name='categoty_list_api'),
              path('onecourse/',views.one_course, name='one_course'),
-          path(
-    'courses/<int:pk>/',
+          path('courses/<int:pk>/',
     views.CourseDetailAPIView.as_view(),
     name='course_detail_api'
 ),]
