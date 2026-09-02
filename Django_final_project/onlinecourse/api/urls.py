481725998cac7d8ca9f75de608335657c2b4ebb4
kalkidan <kaletamene90@gmail.com>
2026-09-02 04:09:00 -0700

    Update course list API endpoint to use CourseListCreateAPIView for improved functionality

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
+urlpatterns=[path(
+    'courses/',
+    views.CourseListCreateAPIView.as_view(),
+    name='course_list_create_api'
+),
              path('category/',views.category_list_api, name='categoty_list_api'),
              path('onecourse/',views.one_course, name='one_course'),
           path('courses/<int:pk>/',views.CourseDetailAPIView.as_view(),name='course_detail_api'
