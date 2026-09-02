be841c7df30a2adb146cc03e1ce0273f78d60883
kalkidan <kaletamene90@gmail.com>
2026-09-02 02:29:50 -0700

    Fix formatting of URL pattern for CourseDetailAPIView for consistency

--- Django_final_project/onlinecourse/api/urls.py
+++ Django_final_project/onlinecourse/api/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns=[path('courses/',views.course_list_api,name="course_list_api"),
              path('category/',views.category_list_api, name='categoty_list_api'),
              path('onecourse/',views.one_course, name='one_course'),
-          path('courses/<int:pk>/',
-    views.CourseDetailAPIView.as_view(),
+          path('courses/<int:pk>/',views.CourseDetailAPIView.as_view(),
     name='course_detail_api'
 ),]
