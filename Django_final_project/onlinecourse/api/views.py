28e2c82aae62a12ed24ea533370642ee26dd7b81
kalkidan <kaletamene90@gmail.com>
2026-09-04 02:22:25 -0700

    Add IsInstructorOrReadOnly permission to enroll action in CourseViewSet

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -94,7 +94,7 @@ class CourseViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(instructor=self.request.user)
-    @action(detail=True,methods=['post'],permission_classes=[IsAuthenticated])
+    @action(detail=True,methods=['post'],permission_classes=[IsAuthenticated,IsInstructorOrReadOnly])
     def enroll(self, request, pk=None):
      course = self.get_object()
 
