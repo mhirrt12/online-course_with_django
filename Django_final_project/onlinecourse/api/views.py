d9d6803c85eda61bc1a94eecfc7a0cdce5f32ca2
kalkidan <kaletamene90@gmail.com>
2026-09-04 01:01:13 -0700

    Fix import statement for django.contrib in views.py

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -4,13 +4,14 @@ from rest_framework.response import Response
 from course.models import Course,Category
 from .serializers import CourseSerializer,CategorySerializer,CourseSerializer2
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework.decorators import (api_view,permission_classes,action)
+from rest_framework.decorators import (api_view,permission_classes,action )
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsInstructorOrReadOnly
 from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from django.contrib
 # @api_view(['GET','POST'])
 # @permission_classes([IsAuthenticated,IsInstructorOrReadOnly])
 # def  course_list_api(request):
