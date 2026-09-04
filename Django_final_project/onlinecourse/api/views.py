5ecf89ab7e1002ad55fc94db84fa0b0290e0ac67
kalkidan <kaletamene90@gmail.com>
2026-09-04 01:51:18 -0700

    Add view_enrolled_courses API endpoint with authentication

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -65,6 +65,8 @@ def login (request):
                         status=status.HTTP_401_UNAUTHORIZED)
     token, created = Token.objects.get_or_create(user=user)
     return Response({"message":"Login successful. ", "token":token.key})
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def view_enrolled_courses(self,request,pk=None):
         course = Course.get_or_404(Course,pk=pk, students=request.user)
         serializer = UserSerializer(enrolled_students, many=True)
