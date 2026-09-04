540503bc061e576f5349d224974bc110ff40a8ff
kalkidan <kaletamene90@gmail.com>
2026-09-04 03:40:38 -0700

    Add RegisterSerializer for user registration with password handling

--- Django_final_project/onlinecourse/api/serializers.py
+++ Django_final_project/onlinecourse/api/serializers.py
@@ -17,3 +17,20 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+
+        return user
