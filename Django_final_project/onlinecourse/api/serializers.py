09133ae702d2ba8caa4dbce15a8374984954d6ed
kalkidan <kaletamene90@gmail.com>
2026-09-05 00:36:37 -0700

    Add password2 field to RegisterSerializer for user registration

--- Django_final_project/onlinecourse/api/serializers.py
+++ Django_final_project/onlinecourse/api/serializers.py
@@ -19,19 +19,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email']
 class RegisterSerializer(serializers.ModelSerializer):
+      password2 = serializers.CharField(write_only=True)
 
-    class Meta:
+      class Meta:
         model = User
         fields = ['username', 'email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
-    def validate_password(self, value):
+      def validate_password(self, value):
         validate_password(value)
         return value
 
-    def create(self, validated_data):
+      def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
