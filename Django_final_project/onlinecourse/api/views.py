eade4a481b77bd034f68477f6fcfbc45967395d7
kalkidan <kaletamene90@gmail.com>
2026-09-08 02:25:20 -0700

    Fix duplicate import of DjangoFilterBackend in views.py

--- Django_final_project/onlinecourse/api/views.py
+++ Django_final_project/onlinecourse/api/views.py
@@ -1,6 +1,7 @@
 from django.http import request
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets, filters,DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 from rest_framework.response import Response
 from course.models import Course,Category
